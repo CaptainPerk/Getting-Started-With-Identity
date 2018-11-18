9f1e74c5c725133bf8491fca78b33402da0829cc
Jon Perkins <captainperk81@gmail.com>
2018-11-18 16:47:27 -0500

    Enumerating User Accounts

---
+++ Users/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Users.Models;
+
+namespace Users.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public ViewResult Index() => View(_userManager.Users);
+    }
+}
